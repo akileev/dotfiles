b01bdb6a61c8e54444ad6c32808cd5626421322e
= <=>
2015-04-21 09:48:37 +0500

    Add cusom key bindings.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,12 @@
 import XMonad
+import XMonad.Util.EZConfig (additionalKeys)
 
-main = xmonad defaultConfig
-	{ modMask = mod2Mask
-	, terminal = 'urxvt'
-	}
+myModMask = mod1Mask
+
+myKeys = [ ((myModMask, xK_p), spawn "dmenu_run -b")
+	, ((myModMask, xK_q), spawn "killall dzen2; xmonad --recompile; xmonad --restart")]
+
+main = xmonad $ defaultConfig
+	{ modMask = myModMask
+	, terminal = "urxvt"
+	} `additionalKeys` myKeys
