45c3b79d73a664fcb956c9db7c26a867264d3809
= <=>
2015-04-21 10:54:30 +0500

    Add custom layouts.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,12 +1,35 @@
 import XMonad
 import XMonad.Util.EZConfig (additionalKeys)
 
+-- Layouts
+import XMonad.Layout.Spacing
+import XMonad.Layout.Grid
+import XMonad.Layout.NoBorders
+
+-- Hooks
+import Data.Ratio ((%))
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.SetWMName
+
 myModMask = mod1Mask
 
 myKeys = [ ((myModMask, xK_p), spawn "dmenu_run -b")
 	, ((myModMask, xK_q), spawn "killall dzen2; xmonad --recompile; xmonad --restart")]
 
+mySpacing = 5
+
+myLayout = spacing mySpacing $ smartBorders ( tiled ||| Mirror tiled ||| gridded ||| Full )
+	where
+		tiled = Tall nmaster delta ratio
+		nmaster = 1
+		delta = 1/2
+		ratio = 1/2
+		gridded = GridRatio (4/3)
+
 main = xmonad $ defaultConfig
 	{ modMask = myModMask
+	, layoutHook = myLayout
 	, terminal = "urxvt"
+	, handleEventHook  = fullscreenEventHook
+        , startupHook = setWMName "LG3D"
 	} `additionalKeys` myKeys
